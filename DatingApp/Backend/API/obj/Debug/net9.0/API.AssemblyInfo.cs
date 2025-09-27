1cc32caed6c13bf24c862b1be01f20dd923bd607
ialexbpl <spartan03311@gmail.com>
2025-09-27 23:32:40 +0200

    Created empty angular proj

--- DatingApp/Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffce4fd8802c47de026d5b9ee8af59035739d193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9720b452beff884ba68fb70d99de918be9fda21f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
